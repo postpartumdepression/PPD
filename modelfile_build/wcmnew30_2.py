ceed99537b3a4e1b32bdb3a95fd2376caa321cab
shw4002 <43353049+shw4002@users.noreply.github.com>
2019-09-16 16:20:30 -0400

    Update wcmnew30_2.py

--- modelfile_build/wcmnew30_2.py
+++ modelfile_build/wcmnew30_2.py
@@ -643,8 +643,17 @@ if __name__ == "__main__":
 history = pd.DataFrame(new_deliverygw4, columns=['person_id','condition_start_date',"anxiety", "mooddisorder", "organicdisorder", "otherdisorder","schizophrenic", "substance"])
 history = history.fillna(0)
 wcmmodelhistory = pd.merge(wcmmodel,history,on = ['person_id','condition_start_date'], how='left')
-print(wcmmodelhistory.isnull().sum())
+#print(wcmmodelhistory.isnull().sum())
 wcmmodelhistory['ppd'].value_counts()
+
+num_cols = cdrnmodelhistory[['edvisitcount']]
+scaler = StandardScaler()
+num_cols = scaler.fit_transform(num_cols)
+normalize = pd.DataFrame(num_cols)
+normalize.rename(columns={0:'edvisitcount'}, 
+                 inplace=True)
+cdrnmodelhistory['edvisitcount'] = normalize['edvisitcount']
+
 
 wcmmodelhistory.to_csv('/Users/bu/Documents/WCMC/PPD/ppdmodel1907/data/modelfile/wcm/wcmmodel30_2.csv', sep=',',index=0)
 
